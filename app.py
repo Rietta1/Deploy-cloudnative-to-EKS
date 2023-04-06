60aa4636cfaf29eedbf02005102bb858037e692f
Loretta <riettabusiness@gmail.com>
2023-04-06 15:40:24 +0000

    Docker working

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import psutil
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    cpu_percent = psutil.cpu_percent()
-    mem_percent = psutil.virtual_momory().percent
+    cpu_metric = psutil.cpu_percent()
+    mem_metric = psutil.virtual_memory().percent
     Message = None
-    if cpu_percent > 80 or mem_percent > 80:
-        Message = "High CPU or Memory Utilization detected. Please scale up"
-    return f"CPU Utilization: {cpu_percent} and Memory Utilization: {mem_percent}"
+    if cpu_metric > 80 or mem_metric > 80:
+        Message = "High CPU or Memory Detected, scale up!!!"
+    return render_template("index.html", cpu_metric=cpu_metric, mem_metric=mem_metric, message=Message)
 
-if __name__ == '_main_':
-    app.run(debug=True, host='0.0.0.0')
+if __name__=='__main__':
+    app.run(debug=True, host = '0.0.0.0')
